d7bf000b39273aae7de63c262f639cb65a311f1e
mhussainilyas <mhussainilyas7@gmail.com>
2025-07-25 16:30:54 -1200

    practice set 06

---
+++ Practice Sets/practice-set-06.js
@@ -0,0 +1,40 @@
+// ===========================================================================================
+//  Create a program to take full name from user and generate a username start with @,
+//  followed by their full name and ends with underscore followed by the length of full name.
+// ===========================================================================================
+
+// let fullName = prompt("Please 😊 enter your full name: ");
+// let userName = `@${fullName.trim().replaceAll(" ", "").toLowerCase()}_${
+//   fullName.length
+// }`;
+// alert(`Your username is: ${userName}`);
+
+// ==================================================================
+//   Take a string and a character from the user and:
+//    a) count how many times that character appears in the string.
+//    b) Case-Insensitive Version
+//    c) Find All Occurrence Positions
+// ==================================================================
+
+// let str = prompt("Insert some random text: ");
+// let char = prompt("Enter a character: ");
+
+// let charCount = 0;
+
+// for (let index in str) {
+//   if (char.toLowerCase() === str[index].toLowerCase()) {
+//     charCount++;
+//     console.log(`Occurrence Positions: ${index}`);
+//   }
+// }
+
+// console.log(`String you entered: ${str}`);
+// console.log(`Character "${char}" appears ${charCount} times.`);
+
+// ==============================================
+//   Count the words present in a given string.
+// ==============================================
+
+let str = prompt("Insert some random text here: ");
+console.log(`String you entered: ${str}`);
+console.log(`Total words: ${str.split(" ").length}`);
